ba50a9298ce5ea819ad7c7bec17273a8ce82439a
David WELMAN <daviwel@e5r7p1.jnb.42.fr>
2016-06-13 15:36:26 +0200

    -a

---
+++ draw_line.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw_line.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daviwel <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/06/13 15:22:07 by daviwel           #+#    #+#             */
+/*   Updated: 2016/06/13 15:32:55 by daviwel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "wolf.h"
+
+void	draw_vert_line(t_info *info, int x)
+{
+	int	y;
+
+	y = info->draw_start;
+	while (y < info->draw_end)
+	{
+		mlx_pixel_put(info->mlx, info->win, x, y, info->col);
+		y++;
+	}	
+}

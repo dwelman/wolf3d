6ba39c015f412de906d2cc8b0caaf44cf14fd015
Dean DU TOIT <ddu-toit@e5r7p3.jnb.42.fr>
2016-06-14 09:29:25 +0200

    -a

--- draw_line.c
+++ draw_line.c
@@ -6,11 +6,22 @@
 /*   By: daviwel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/13 15:22:07 by daviwel           #+#    #+#             */
-/*   Updated: 2016/06/14 08:09:27 by daviwel          ###   ########.fr       */
+/*   Updated: 2016/06/14 09:29:18 by ddu-toit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf.h"
+
+int     rerender(t_info *info)
+{
+	if (info->mlx)
+		mlx_clear_window(0, info->win);
+	if (info->img)
+		mlx_destroy_image(info->mlx, info->img);
+	info->img = mlx_new_image(info->mlx, WIN_X, WIN_Y);
+	mlx_put_image_to_window(info->mlx, info->win, info->img, 0 ,0);
+	raycast(info);
+}
 
 void	draw_vert_line(t_info *info, int x)
 {
